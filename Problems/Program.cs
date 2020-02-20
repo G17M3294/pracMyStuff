9510e7927a8334481d00e1d352d1698306e8322c
g17m3294 <g17m3294@campus.ru.ac.za>
2020-02-20 14:59:41 +0200

    added csproj, cs sln

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,8 +2,18 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
+		Prefix(string input)
+		{//do it propper
+			int[] numWords = input.split(" ");
+			int stringLength = input.length;
+			
+			
+			string result = stringLength.toString() + "," + numWords.toString() + input;
+			return result;
+		}
+		
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
