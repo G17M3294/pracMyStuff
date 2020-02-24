a80bc11f34a6beac3630201f807aec53da2246db
g17m3294 <g17m3294@campus.ru.ac.za>
2020-02-24 17:02:31 +0200

    committing .travis.yml file

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -3,21 +3,27 @@
 namespace Problems
 {
     public class Program
-    {
-		Prefix(string input)
-		{//do it propper
-			int[] numWords = input.split(" ");
-			int stringLength = input.length;
-			
-			
-			string result = stringLength.toString() + "," + numWords.toString() + input;
-			return result;
-		}
-		
+    {	  		
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-			//Prefix();
+            //Console.WriteLine("Hello World!");
+			Prefix(input);				
+        }
+
+         Prefix(string input)
+        {
+            int stringLength = input.length;
+            int a = 0, myWord = 1;
+
+            while (a <= input.Length - 1)
+            {
+                if (str[a] == ' ' || str[a] == '\n' || str[a] == '\t')
+                {
+                    myWord++;
+                }
+                a++;
+            }
+            Console.Write(stringLength + ",", myWord + input);
         }
     }
 }
